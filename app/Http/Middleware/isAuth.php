8d6097b23b41857ca67d802b5a4291fbf7b71825
Samael Home <samaelortiz2218@gmail.com>
2025-09-03 20:01:26 -0700

    Añadiendo metodo para registrar un usuario en AuthController

---
+++ app/Http/Middleware/isAuth.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace App\Http\Middleware;
+
+use Closure;
+use Illuminate\Http\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Tymon\JWTAuth\Facades\JWTAuth;
+
+class isAuth
+{
+    /**
+     * Handle an incoming request.
+     *
+     * @param  \Closure(\Illuminate\Http\Request): (\Symfony\Component\HttpFoundation\Response)  $next
+     */
+    public function handle(Request $request, Closure $next): Response
+    {
+        // Checamos si hay una cookie llamada 'token'
+       /*  $token = $request->cookie('token');
+
+        if ($token) {
+            // Checa si ese token es válido para algun usuario
+            $user = JWTAuth::setToken($token)->authenticate();
+            if ($user) {
+                return $next($request);
+            }
+        } */
+
+        return $next($request);
+    }
+}
