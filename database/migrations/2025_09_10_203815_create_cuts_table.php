b0bdb8307d4921b578cd0cf45505b5da4b42e599
Samael Medina Laptop <samaelortiz2218@gmail.com>
2025-09-10 13:48:59 -0700

    Creando migracion y modelo de Cut e implementando la relacion con lotes

---
+++ database/migrations/2025_09_10_203815_create_cuts_table.php
@@ -0,0 +1,45 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('cuts', function (Blueprint $table) {
+            $table->id();
+            $table->enum('type', ['x', 'z'])->default('x');
+            $table->date('date');
+            $table->time('time');
+            $table->foreignId('lot_id')->constrained()->cascadeOnDelete();
+            /* 
+            
+            // Los valores que el usurio nos manda
+            $table->float('cash', 10, 2)->nullable()->default(0.0);
+            $table->float('card', 10, 2)->nullable()->default(0.0);
+            // Los valores que sumamos apartir de las ventas del lote
+            $table->float('cash_total', 10, 2)->nullable()->default(0.0);
+            $table->float('card_total', 10, 2)->nullable()->default(0.0);
+            $table->float('total', 10, 2)->nullable()->default(0.0);
+            // Las diferencias que se generan
+            $table->float('cash_difference', 10, 2)->nullable()->default(0.0);
+            $table->float('card_difference', 10, 2)->nullable()->default(0.0);
+            $table->float('total_difference', 10, 2)->nullable()->default(0.0);
+             */
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('cuts');
+    }
+};
