1f3cf316d3889ef43a5f6f73e5c0e13b32070f3c
Samael Home <samaelortiz2218@gmail.com>
2025-09-13 21:53:13 -0700

    Correxion en la tabla de lote - Se agregaron los campos cash y card para colocar el valor acumulado de cada uno - Se implemento el aumento de cada uno en el metodo Store Sale

--- app/Http/Controllers/CutController.php
+++ app/Http/Controllers/CutController.php
@@ -3,7 +3,9 @@
 namespace App\Http\Controllers;
 
 use App\Models\Cut;
+use App\Models\Lot;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Validator;
 
 class CutController extends Controller
 {
@@ -20,7 +22,42 @@ class CutController extends Controller
      */
     public function store(Request $request)
     {
-        //
+        $validator = Validator::make($request->all(), [
+            'type' => 'required|in:x,z',
+            'cash' => 'required',
+            'card' => 'required',
+        ], [
+            'type.required' => 'El tipo es requerido',
+            'type.in' => 'El tipo debe ser x o z',
+            'cash.required' => 'El efectivo es requerido',
+            'card.required' => 'La tarjeta es requerida',
+        ]);
+
+        if ($validator->fails()) {
+            return response()->json([
+                'status' => 'error',
+                'message' => $validator->errors()->first(),
+            ], 422);
+        }
+
+        $lot = Lot::latest()->first();
+        return $lot;
+
+        if ($request->type == 'x') {
+            $cut = Cut::create([
+                'type' => $request->type,
+                'date' => now(),
+                'time' => now(),
+                'product_count' => $lot->product_count,
+                'lot_id' => $lot->id,
+            ]);
+            $cut->cutDetails()->create([
+                'cash' => $request->cash,
+                'card' => $request->card,
+
+            ]);
+        }
+        
     }
 
     /**
